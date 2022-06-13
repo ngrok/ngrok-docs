1d2799e6526bbfc6bccb4b63f6693ac50604fd33
Frederico Hakamine <frederico.hakamine@gmail.com>
2022-06-13 09:47:18 -0500

    migration to ngrok repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,6 +4,8 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+const docsRepo = 'https://github.com/ngrok/ngrok-docs'
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'ngrok documentation',
@@ -16,16 +18,16 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'sudobinbash', // Usually your GitHub org/user name.
-  projectName: 'ngrok-doc', // Usually your repo name.
+  organizationName: 'ngrok', // Usually your GitHub org/user name.
+  projectName: 'ngrok-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'pt-br'],
-  },
+  // i18n: {
+  //   defaultLocale: 'en',
+  //   locales: ['en', 'pt-br'],
+  // },
 
   presets: [
     [
@@ -35,30 +37,31 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
-          editUrl: 'https://github.com/sudobinbash/ngrok-doc/edit/master',
+          editUrl: `${docsRepo}/edit/master`,
         },
         blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
       }),
-    ],[
-      'redocusaurus',
-      {
-        // Plugin Options for loading OpenAPI files
-        specs: [
-          {
-            spec: 'openapi/ngrok_openapi.yml',
-            route: '/docs/api/',
-          },
-        ],
-        // Theme Options for modifying how redoc renders them
-        theme: {
-          // Change with your site colors
-          primaryColor: '#1890ff',
-        },
-      },
     ],
+    // [
+    //   'redocusaurus',
+    //   {
+    //     // Plugin Options for loading OpenAPI files
+    //     specs: [
+    //       {
+    //         spec: 'openapi/ngrok_openapi.yml',
+    //         route: '/docs/api/',
+    //       },
+    //     ],
+    //     // Theme Options for modifying how redoc renders them
+    //     theme: {
+    //       // Change with your site colors
+    //       primaryColor: '#1890ff',
+    //     },
+    //   },
+    // ],
   ],
 
   themeConfig:
@@ -72,12 +75,12 @@ const config = {
           srcDark: 'img/ngrok-white.svg',
         },
         items: [
-          {to: '/', label: 'Docs', position: 'left'},
-          {to: '/docs/api/', label: 'API', position: 'left'},
-          {
-            type: 'localeDropdown',
-            position: 'right',
-          },
+          // {to: '/', label: 'Docs', position: 'left'},
+          // {to: '/docs/api/', label: 'API', position: 'left'},
+          // {
+          //   type: 'localeDropdown',
+          //   position: 'right',
+          // },
           // {
           //   type: 'doc',
           //   docId: 'intro',
@@ -86,7 +89,7 @@ const config = {
           // },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/sudobinbash/ngrok-doc',
+            href: docsRepo,
             label: 'GitHub',
             position: 'right',
           },
@@ -105,11 +108,48 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
+            title: 'Learn',
             items: [
               {
-                label: 'Index',
-                to: '/docs/intro',
+                label: 'Get Started',
+                to: '/guides/getting-started',
+              },
+              {
+                label: 'Sign up',
+                to: 'https://dashboard.ngrok.com/signup',
+              },
+              {
+                label: 'Login',
+                to: 'https://dashboard.ngrok.com/login',
+              },
+              {
+                label: 'Download',
+                to: 'https://ngrok.com/download',
+              },
+            ],
+          },
+          {
+            title: 'Service',
+            items: [
+              {
+                label: 'Product',
+                to: 'https://ngrok.com/product',
+              },
+              {
+                label: 'Pricing',
+                to: 'https://ngrok.com/pricing',
+              },
+              {
+                label: 'Docs',
+                to: 'https://ngrok.com/docs',
+              },
+              {
+                label: 'Trust Portal',
+                to: 'https://trust.ngrok.com',
+              },
+              {
+                label: 'Service Status',
+                to: 'https://status.ngrok.com',
               },
             ],
           },
@@ -119,24 +159,69 @@ const config = {
               {
                 label: 'Slack',
                 href: 'https://ngrok.com/slack',
-              }
+              },
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/ngrok',
+              },
+              {
+                label: 'File Doc Issues',
+                href: `${docsRepo}/issues`,
+              },
+            ],
+          },
+          {
+            title: 'Legal',
+            items: [
+              {
+                label: 'Abuse',
+                to: 'https://ngrok.com/abuse',
+              },
+              {
+                label: 'DPA',
+                to: 'https://ngrok.com/dpa',
+              },
+              {
+                label: 'Privacy',
+                to: 'https://ngrok.com/privacy',
+              },
+              {
+                label: 'Security',
+                to: 'https://ngrok.com/security',
+              },
+              {
+                label: 'Terms of service',
+                to: 'https://ngrok.com/tos',
+              },
             ],
           },
           {
             title: 'More',
             items: [
               {
+                label: 'Careers',
+                to: 'https://ngrok.com/careers',
+              },
+              {
                 label: 'Blog',
-                to: '/blog',
+                to: 'https://blog.ngrok.com',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/ngrokHQ',
+              },
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/company/ngrok',
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/sudobinbash/ngrok-doc',
+                href: 'https://github.com/ngrok',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} ngrok. Built with Docusaurus ❤️.`,
+        copyright: `Copyright © ${new Date().getFullYear()} ngrok. <a href="https://blog.ngrok.com/why-we-use-docusaurus">Built with Docusaurus</a> ❤️.`,
       },
       prism: {
         theme: lightCodeTheme,
