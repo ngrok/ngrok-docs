92414c61fa06f15d9a2fdf2d3b281302ca904b36
Frederico Hakamine <frederico.hakamine@gmail.com>
2022-10-17 19:06:51 -0500

    fixed algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,11 +71,10 @@ const config = {
           srcDark: 'img/ngrok-white.svg',
         },
         items: [
-          // TODO: fix algolia
-          // {
-          //   type: 'search',
-          //   position: 'left',
-          // },
+          {
+            type: 'search',
+            position: 'left',
+          },
           {
             label: 'go to ngrok.com',
             to: 'https://ngrok.com',
@@ -131,9 +130,9 @@ const config = {
         ],
       },
       algolia: {
-        appId: 'SPPRT3GDNI',
-        apiKey: 'e02fb8e0c4d8c7968396981d7ecb9fa8',
-        indexName: 'dev_ngrok',
+       appId: 'SPPRT3GDNI',
+       apiKey: 'e02fb8e0c4d8c7968396981d7ecb9fa8',
+       indexName: 'dev_ngrok',
       },
       hubspot: {
         accountId: 21124867,
