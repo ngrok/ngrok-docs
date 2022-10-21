58174994f09333570443228d9bce9af39fcd00d1
Frederico Hakamine <frederico.hakamine@gmail.com>
2022-10-20 19:23:33 -0500

    optimize images, logo, and css load

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,6 @@ const config = {
       ],
       image: 'img/ngrok-docs-opengraph.png',
       navbar: {
-        title: '| docs',
         logo: {
           alt: 'ngrok',
           src: 'img/ngrok-black.svg',
