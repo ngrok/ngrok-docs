1a237fcd67057c3905e1777ae6a05f287cfd73cc
faonbr <faonbr@gmail.com>
2022-10-26 16:57:17 -0300

    obfuscating PagerDuty

--- sidebars.js
+++ sidebars.js
@@ -51,10 +51,11 @@ const sidebars = {
       // TODO: @sudobinbash: Change to autogen (for scalability)
       items: [ 
         { label: 'Bitbucket', type: 'doc', id: 'integrations/bitbucket/webhooks', },
-        { label: 'Box', type: 'doc', id: 'integrations/box/webhooks', },
+        //{ label: 'Box', type: 'doc', id: 'integrations/box/webhooks', },
         { label: 'Brex', type: 'doc', id: 'integrations/brex/webhooks', },
-        { label: 'Calendly', type: 'doc', id: 'integrations/calendly/webhooks', },
+        //{ label: 'Calendly', type: 'doc', id: 'integrations/calendly/webhooks', },
         { label: 'Circle CI', type: 'doc', id: 'integrations/circleci/webhooks', },
+        //{ label: 'Contentful', type: 'doc', id: 'integrations/contentful/webhooks', },
         { label: 'DocuSign', type: 'doc', id: 'integrations/docusign/webhooks', },
         { label: 'Facebook', type: 'doc', id: 'integrations/facebook/webhooks', },
         { label: 'Facebook Messenger', type: 'doc', id: 'integrations/facebook-messenger/webhooks', },
