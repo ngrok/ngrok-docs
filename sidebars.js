bbe19362b7224055162dd0158c871415b0ef887e
Thomas Kirk <thomas@ngrok.com>
2022-11-21 11:48:48 -0500

    Add SSO Docs

--- sidebars.js
+++ sidebars.js
@@ -62,6 +62,7 @@ const sidebars = {
         { label: 'Circle CI', type: 'doc', id: 'integrations/circleci/webhooks', },
         { label: 'Coinbase', type: 'doc', id: 'integrations/coinbase/webhooks', },
         { label: 'Contentful', type: 'doc', id: 'integrations/contentful/webhooks', },
+        { label: 'Curity', type: 'doc', id: 'integrations/curity/sso-oidc', },
         { label: 'DocuSign', type: 'doc', id: 'integrations/docusign/webhooks', },
         { label: 'Dropbox', type: 'doc', id: 'integrations/dropbox/webhooks', },
         { label: 'Facebook', type: 'doc', id: 'integrations/facebook/webhooks', },
