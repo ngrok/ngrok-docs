317cef59388d27b4336570355d76792af3e6fa6b
ben <ben@ngrok.com>
2022-11-28 17:02:08 -0800

    add azure ad draft

--- sidebars.js
+++ sidebars.js
@@ -54,6 +54,7 @@ const sidebars = {
       // TODO: @sudobinbash: Change to autogen (for scalability)
       items: [
         { label: 'Aftership', type: 'doc', id: 'integrations/aftership/webhooks', },
+        { label: 'Azure AD', type: 'doc', id: 'integrations/azuread/sso-saml', },
         { label: 'Bitbucket', type: 'doc', id: 'integrations/bitbucket/webhooks', },
         { label: 'Box', type: 'doc', id: 'integrations/box/webhooks', },
         { label: 'Brex', type: 'doc', id: 'integrations/brex/webhooks', },
