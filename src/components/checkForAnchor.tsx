5468d16f3e590912771bf686865d3972dbf883dd
andrew van beek <andrewvanbeek@andrews-air.lan>
2022-12-28 17:32:31 -0600

    fixes build for anchor link component

--- src/components/checkForAnchor.tsx
+++ src/components/checkForAnchor.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from '@docusaurus/router';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default ({ data }) => {
-    var anchor = window.location.href.split("#")[1]
-    var els = document.querySelectorAll(`a[href='#${anchor}']`)
-    if(els[1]) {
-        els[1].scrollIntoView() 
-    }
-   // els.scrollIntoView() 
-    return (
-        <span style={{"display": "none"}}>
-        </span>
-      );
-  }
+    const location = useLocation();
+
+    useEffect(() => {
+        var els = document.querySelectorAll(`a[href='${location.hash}']`)
+        if (els.length > 0) {
+            let nodeListArrayForm = Array.from(els)
+            var anchorLink = nodeListArrayForm.find(function (atag) { return atag.className == "hash-link" })
+            if (anchorLink) {
+                anchorLink.scrollIntoView()
+            }
+        }
+    }, [location]);
+
+    return <BrowserOnly>
+        {() => <span style={{ "display": "none" }}></span>}
+    </BrowserOnly>
+}
