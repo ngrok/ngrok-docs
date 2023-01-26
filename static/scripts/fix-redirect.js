2f55a36b32dabb7df42dbf6b0c4fc33f0ca893e4
Frederico Hakamine <frederico.hakamine@gmail.com>
2023-01-26 16:14:11 -0600

    Update fix-redirect.js

--- static/scripts/fix-redirect.js
+++ static/scripts/fix-redirect.js
@@ -1,12 +1,3 @@
 
 // Just for exact match
-// if(window.location.pathname == "/docs/ngrok-link") window.location.href = `${window.location.origin}/docs/cloud-edge`
-
-//if you want handle any instance with paths.  
-if(window.location.pathname.includes("/docs/ngrok-link")) {
-    var regex = /\/docs\/ngrok-link/g;
-    var replacement = "/docs/cloud-edge";
-    var currentHref = window.location.href
-    var newLocation = currentHref.replace(regex, replacement);
-    window.location.href = newLocation
-}
+if(window.location.pathname.includes == "/docs/ngrok-link") window.location.href = `${window.location.origin}/docs/cloud-edge`
