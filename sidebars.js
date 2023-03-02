0b1406f5be4eabac5140e8322259311be3f0a486
Felippe Augusto de Oliveira Neto <faonbr@gmail.com>
2023-03-02 15:47:21 -0300

    Cisco Webex webhook doc (#199)

--- sidebars.js
+++ sidebars.js
@@ -67,6 +67,7 @@ const sidebars = {
         { label: 'Castle', type: 'doc', id: 'integrations/castle/webhooks', },
         { label: 'Chargify', type: 'doc', id: 'integrations/chargify/webhooks', },
         { label: 'Circle CI', type: 'doc', id: 'integrations/circleci/webhooks', },
+        { label: 'Cisco Webex', type: 'doc', id: 'integrations/webex/webhooks', },
         { label: 'Clerk', type: 'doc', id: 'integrations/clerk/webhooks', },
         { label: 'Coinbase', type: 'doc', id: 'integrations/coinbase/webhooks', },
         { label: 'Contentful', type: 'doc', id: 'integrations/contentful/webhooks', },
