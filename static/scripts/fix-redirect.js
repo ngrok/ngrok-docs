74587e0bc35869d74df66e10fc919cc2b0a2f8f1
Niji <nijikokun@gmail.com>
2023-06-23 16:27:54 -0700

    fix: move redirects to fix-redirect.js

--- static/scripts/fix-redirect.js
+++ static/scripts/fix-redirect.js
@@ -1,3 +1,10 @@
+const path = window.location.pathname
+const goto = (path) => window.location.href = `${window.location.origin}${path}`
 
 // Just for exact match
-if(window.location.pathname.includes == "/docs/ngrok-link") window.location.href = `${window.location.origin}/docs/cloud-edge`
+if (path.includes("/docs/ngrok-link")) goto(`/docs/cloud-edge`)
+if (path.includes("/docs/api/api-clients")) goto(`/docs/api/`)
+if (path.includes("/docs/api/client-libraries")) goto(`/docs/api/`)
+if (path.includes("/docs/api/terraform")) goto(`/docs/api/`)
+if (path.includes("/docs/platform/api")) goto(`/docs/api/`)
+if (path.includes("/docs/api/api-clients")) goto(`/docs/api/`)
