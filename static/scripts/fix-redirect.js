2cad0eb48b0de8d1612a3c41f3f5aea798597b2a
Niji <nijikokun@gmail.com>
2023-06-23 16:37:13 -0700

    fix: redirect to hash

--- static/scripts/fix-redirect.js
+++ static/scripts/fix-redirect.js
@@ -1,10 +1,11 @@
 const path = window.location.pathname
-const goto = (path) => window.location.href = `${window.location.origin}${path}`
+const goto = (path) => {
+    window.location.href = `${window.location.origin}${path}`
+}
 
 // Just for exact match
 if (path.includes("/docs/ngrok-link")) goto(`/docs/cloud-edge`)
-if (path.includes("/docs/api/api-clients")) goto(`/docs/api/`)
-if (path.includes("/docs/api/client-libraries")) goto(`/docs/api/`)
-if (path.includes("/docs/api/terraform")) goto(`/docs/api/`)
+if (path.includes("/docs/api/api-clients")) goto(`/docs/api/#client-libraries`)
+if (path.includes("/docs/api/client-libraries")) goto(`/docs/api/#client-libraries`)
+if (path.includes("/docs/api/terraform")) goto(`/docs/api/#terraform-provider`)
 if (path.includes("/docs/platform/api")) goto(`/docs/api/`)
-if (path.includes("/docs/api/api-clients")) goto(`/docs/api/`)
