dd75aaaf9ee063500edb26ec5a7c1e72c3665362
Mohamed Osman Ahmed <mohamed@ngrok.com>
2024-03-18 21:02:03 +0000

    update variable interpolation sections

--- src/components/VariableInterpolation.tsx
+++ src/components/VariableInterpolation.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 
-const VariableInterpolation = () => (
-	<>
-		<h3>Variable Interpolation</h3>
-		<p>
-			You may interpolate variables into header values. Variables are
-			interpolated into headers with JSONPath expressions surrounded by the `${}
-			` syntax.
-		</p>
-		<p>
-			For example, to include geographical data about the client IP that
-			initiated the request, you may construct a header value like so.
-		</p>
-	</>
-);
+const VariableInterpolation = () => {
+	const templateSyntax = "${}";
+
+	return (
+		<>
+			<h3>Variable Interpolation</h3>
+			<p>
+				You may interpolate variables into header values. Variables are
+				interpolated into headers with JSONPath expressions surrounded by{" "}
+				<code>{templateSyntax}</code> syntax.
+			</p>
+			<p>
+				For example, to include geographical data about the client IP that
+				initiated the request, you may construct a header value like so.
+			</p>
+		</>
+	);
+};
 
 export default VariableInterpolation;
