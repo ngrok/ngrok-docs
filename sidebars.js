ee589834190fe45cdb0da67352f20e5405bf7033
Salil Subbakrishna <s.subbakrishna@ngrok.com>
2024-04-08 08:46:58 -0700

    Switching sidebars

--- sidebars.js
+++ sidebars.js
@@ -175,8 +175,7 @@ const sidebars = {
 				"k8s/index",
 				"k8s/how-it-works",
 				"k8s/with-edges",
-				"k8s/getting-started-kic",
-				"k8s/getting-started-gwapi",
+				"k8s/getting-started",
 			],
 		},
 		{
