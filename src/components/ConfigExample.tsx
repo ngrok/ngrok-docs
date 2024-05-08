b283e8ab5519fc7a5068dd5437758527e5a2f5f4
Harrison Katz <hjkatz03@gmail.com>
2024-05-08 15:55:39 -0500

    Update ConfigExample.tsx

--- src/components/ConfigExample.tsx
+++ src/components/ConfigExample.tsx
@@ -5,21 +5,25 @@ import TabItem from "@theme/TabItem";
 import YAML from "yaml";
 
 export default function ConfigExample({ config }) {
-	const jsonExample = {
-		// key ordering
-		"//": "...snippet...",
-		...config,
-	};
+	const yamlConfig = YAML.stringify(config, {
+		indent: 2,
+		directives: true,
+		defaultKeyType: "PLAIN",
+		defaultStringType: "QUOTE_DOUBLE",
+	});
+
+	const jsonConfig = JSON.stringify(config, null, 2);
+	
 	return (
 		<Tabs groupId="config_example" queryString="config">
 			<TabItem value="YAML" label="YAML">
 				<CodeBlock language="yaml">
-					{"---\n# snippet\n" + YAML.stringify(config)}
+					{"# snippet\n" + yamlConfig}
 				</CodeBlock>
 			</TabItem>
 			<TabItem value="JSON" label="JSON">
 				<CodeBlock language="yaml">
-					{"// snippet\n" + JSON.stringify(jsonExample, null, 2)}
+					{"// snippet\n" + jsonConfig}
 				</CodeBlock>
 			</TabItem>
 		</Tabs>
