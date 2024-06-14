87a67a1c3713c7c3efb7db8b560974e9540d1520
Cody A Price <cody@ngrok.com>
2024-06-14 10:50:57 -0500

    fully setup typescript, fix tsc errors, add tsc check to gh pr workflow

--- src/components/ErrorDetails.tsx
+++ src/components/ErrorDetails.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import ErrorBoundary from "@docusaurus/ErrorBoundary";
 import loadable from "@loadable/component";
 
-export default function ErrorDetails({ error }) {
+type ErrorDetailsProps = {
+	error: `err_ngrok_${string}` | `err_ngrok_${number}` | (string & {});
+}
+
+export default function ErrorDetails({ error }: ErrorDetailsProps) {
 	const Error = loadable(
 		() => import("/docs/errors/details/_" + error + ".md"),
 	);
