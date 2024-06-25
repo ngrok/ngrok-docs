95000bf6d8382de427d4099932a123fa1dc2dddf
Cody A Price <cody@ngrok.com>
2024-06-25 15:18:39 -0500

    force bad code, should auto run prettier

--- src/components/CelTemplateSyntaxExplanation.tsx
+++ src/components/CelTemplateSyntaxExplanation.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
 const CelTemplateSyntaxExplanation = () => {
-	const templateSyntax = "${}";
+	const templateSyntax = '${}'
 
 	return (
 		<>
