580d55b3951e4a5bdc470b635320616be8aaf6d2
Cody A Price <cody@ngrok.com>
2025-01-27 19:28:36 -0600

    um, that's json bb

--- src/components/ConfigExample.tsx
+++ src/components/ConfigExample.tsx
@@ -59,7 +59,7 @@ export default function ConfigExample({
 				>
 					{() => (
 						<DocsCodeBlock
-							language="yaml"
+							language="json"
 							metastring={jsonMetastring}
 							title={title}
 							icon={icon}
