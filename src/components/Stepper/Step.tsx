5736bc9c11ee6ee7a2aff342ab8c4f3ab3caf898
Shaquil Hansford <liuqahs15@gmail.com>
2025-02-03 15:41:25 -0500

    Remove comments

--- src/components/Stepper/Step.tsx
+++ src/components/Stepper/Step.tsx
@@ -17,11 +17,6 @@ const Step = ({ children, isLast = false }: StepProps) => {
 				"relative p-4 pt-0 md:border-l-4 md:pb-[4.5rem] md:pl-6",
 			)}
 		>
-			{/* <div style={{ margin: "40px" }}>{children}</div>; */}
-			{/* <Panel.Header className="relative px-0 text-gray-600 md:mb-6 md:p-4 md:py-0">
-				<h2 className="text-strong -ml-0.5 mb-1 p-0 text-4xl font-medium">{title}</h2>
-				<p className="mb-4 text-base">Hi</p>
-			</Panel.Header> */}
 			<Panel.Body className={cx("rounded-lg border border-card p-4 md:p-6")}>
 				{children}
 			</Panel.Body>
