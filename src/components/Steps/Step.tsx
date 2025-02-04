e3ec26cd4d9109db37152e080529effb2ece23d4
Shaquil Hansford <liuqahs15@gmail.com>
2025-02-04 09:34:11 -0500

    Ready for review

--- src/components/Steps/Step.tsx
+++ src/components/Steps/Step.tsx
@@ -1,5 +1,6 @@
-import type { WithStyleProps } from "@ngrok/mantle/types";
-import { Children, type ReactNode } from "react";
+import { cx } from "@ngrok/mantle/cx";
+import type { ReactNode } from "react";
+import { Panel } from "./Panel";
 
 const StickNum = ({ num, isLast }: { num: number; isLast: boolean }) => {
 	return (
@@ -53,26 +54,37 @@ const StickNum = ({ num, isLast }: { num: number; isLast: boolean }) => {
 		</div>
 	);
 };
-type StepListProps = {
-	children: ReactNode;
+
+export type StepProps = {
+	num: number;
+	body: ReactNode[];
+	title: string;
+	isLast: boolean;
 };
 
-const StepList = ({ children, className }: StepListProps & WithStyleProps) => {
-	const arrayChildren = Children.toArray(children);
-
+const Step = ({ body, isLast = false, num, title }: StepProps) => {
 	return (
-		<div style={{ margin: "10px" }} className={className}>
-			{arrayChildren.map((child, index) => (
-				<div key={index} className="relative m-0.5">
-					<StickNum
-						num={index + 1}
-						isLast={index === arrayChildren.length - 1}
-					/>
-					{child}
-				</div>
-			))}
-		</div>
+		<>
+			<StickNum num={num} isLast={isLast} />
+			<Panel
+				className={cx(
+					!isLast
+						? "border-gray-200 dark-high-contrast:border-black high-contrast:border-black"
+						: "border-transparent",
+					"relative p-4 pt-0 md:border-l-4 md:pb-[4.5rem] md:pl-6",
+				)}
+			>
+				<Panel.Header className="relative px-0 text-gray-600 md:mb-6 md:p-4 md:py-0">
+					<h2 className="-ml-0.5 mb-1 p-0 text-4xl font-medium text-strong">
+						{title}
+					</h2>
+				</Panel.Header>
+				<Panel.Body className={cx("rounded-lg border border-card p-4 md:p-6")}>
+					{body}
+				</Panel.Body>
+			</Panel>
+		</>
 	);
 };
 
-export default StepList;
+export default Step;
---
+++ src/components/Steps/index.tsx
@@ -0,0 +1,57 @@
+import type { WithStyleProps } from "@ngrok/mantle/types";
+import { Children, type ReactNode } from "react";
+import Step from "./Step";
+
+const getStepData = (arrayChildren: Array<any>): Array<StepData> => {
+	const stepData: Array<StepData> = [];
+	let stepNum = -1;
+	arrayChildren.forEach((child: any, i) => {
+		if (child?.type?.name === "h2") {
+			stepNum++;
+			return stepData.push({
+				title: child.props.children,
+				body: [],
+			});
+		}
+		if (stepNum < 0)
+			throw new Error(
+				"When using StepList, each step must be an h2. Did you forget to put an h2 above your first step?",
+			);
+		return stepData[stepNum]?.body.push(child);
+	});
+	return stepData;
+};
+
+type StepListProps = {
+	children: ReactNode;
+};
+
+type StepData = {
+	title: string;
+	body: Array<any>;
+};
+
+const StepList = ({ children }: StepListProps & WithStyleProps) => {
+	const arrayChildren = Children.toArray(children);
+	const stepData = getStepData(arrayChildren);
+
+	return (
+		<div className="mt-8 sm:ml-5">
+			{stepData.map((step, index) => {
+				const isLast = index === stepData.length - 1;
+				return (
+					<div key={index} className="relative">
+						<Step
+							num={index + 1}
+							isLast={isLast}
+							body={step.body}
+							title={step.title}
+						/>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
+
+export default StepList;
