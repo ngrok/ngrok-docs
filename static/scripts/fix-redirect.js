47785ec524fcca76ca19d0358f6c7e724edc7298
Shaquil Hansford <liuqahs15@gmail.com>
2025-02-06 13:00:15 -0500

    Fixed redirects

--- static/scripts/fix-redirect.js
+++ static/scripts/fix-redirect.js
@@ -1,4 +1,3 @@
-import { iaRestructureRedirects } from "./other-redirects/ia-restructure";
 
 const fromExact = (from) => (path) => [ from, path === from ]               // [xyz]
 const fromIncludes = (from) => (path) => {
@@ -20,7 +19,6 @@ const toReplace = (to) => (path, from) => path.replace(from, to)            // a
 //  [0 - from]: (path) => [matchStr, boolean (true for match, false for do not match)]
 //  [1 - to]:  (path, from) => string (returned value becomes the new path)
 const redirects = [
-    ... iaRestructureRedirects,
     [ fromIncludes(`/docs/1`), `/docs/` ],
     [ fromIncludes(`/docs/2`), `/docs/` ],
     [ fromIncludes(`/docs/ngrok-link`), `/docs/universal-gateway/overview/` ],
@@ -165,8 +163,8 @@ const redirects = [
     [ fromIncludes(`/docs/cloud-edge/modules/`), `/docs/http/#modules` ],
     [ fromIncludes(`/docs/cloud-edge/observability/`), `/docs/http/#observability` ],
     [ fromIncludes(`/docs/cloud-edge/pops/`), `/docs/universal-gateway/points-of-presence/` ],
-    [ fromIncludes(`/docs/cloud-edge/zero-knowledge-tls/`), `/docs/agent/agent-tls-termination/` ],
-    [ fromIncludes(`/docs/tls/tls/termination/`), `/docs/agent/agent-tls-termination/` ],
+    [ fromIncludes(`/docs/cloud-edge/zero-knowledge-tls/`), `/docs/traffic-policy/actions/terminate-tls/` ],
+    [ fromIncludes(`/docs/tls/tls/termination/`), `/docs/traffic-policy/actions/terminate-tls/` ],
     [ fromIncludes(`/docs/cloud-edge/`), `/docs/universal-gateway/overview/` ],
     [ fromIncludes(`/docs/integrations/home-assistant/home-assistant`), `/docs/integrations/home-assistant/home-assistant-with-ngrok` ],
 
@@ -245,8 +243,48 @@ const redirects = [
     [ fromExact(`/docs/traffic-policy/getting-started/`), `/docs/traffic-policy/getting-started/agent-endpoints/cli` ],
 
     // JAN 2025
-    [ fromIncludes(`/docs/tls/tls-termination/`), `/docs/agent/agent-tls-termination/` ],
+    [ fromIncludes(`/docs/tls/tls-termination/`), `/docs/traffic-policy/actions/terminate-tls/` ],
     [ fromIncludes(`/docs/traffic-policy/templates/`), `/docs/traffic-policy/examples/` ],
+    
+    /*IA Restructure redirects*/
+    [ fromIncludes('/docs/tls/termination/agent-tls-termination/'), '/docs/agent/agent-tls-termination/'],
+    [ fromIncludes('/docs/concepts/'), '/docs/overview/'],
+        // HTTP Redirects
+    [ fromIncludes('/docs/http/basic-auth'), '/docs/traffic-policy/actions/basic-auth/'],
+    [ fromIncludes('/docs/http/circuit-breaker'), '/docs/traffic-policy/actions/circuit-breaker/'],
+    [ fromIncludes('/docs/http/compression/'), '/docs/traffic-policy/actions/compress-response/'],
+    [ fromIncludes('/docs/http/ip-restrictions/'), '/docs/traffic-policy/actions/restrict-ips/'],
+    [ fromIncludes('/docs/http/mutual-tls/'), '/docs/traffic-policy/actions/terminate-tls/#enabling-mutual-tls'],
+    [ fromIncludes('/docs/http/oauth/'), '/docs/traffic-policy/actions/oauth'],
+    [ fromIncludes('/docs/http/openid-connect/'), '/docs/traffic-policy/actions/oidc'],
+    [ fromIncludes('/docs/http/request-headers/'), '/docs/traffic-policy/actions/add-headers'],
+    [ fromIncludes('/docs/http/response-headers/'), '/docs/traffic-policy/actions/custom-response'],
+    [ fromIncludes('/docs/http/saml/'), '/docs/traffic-policy/actions/saml'],    
+        // Network Edge
+    [ fromIncludes('/docs/network-edge/'), '/docs/universal-gateway/edges'],
+    [ fromIncludes('/docs/network-edge/edges'), '/docs/universal-gateway/edges'],
+    [ fromIncludes('/docs/network-edge/cloud-endpoints'), '/docs/universal-gateway/cloud-endpoints'],
+    [ fromIncludes('/docs/network-edge/domains-and-tcp-addresses'), '/docs/universal-gateway/domains'],
+    [ fromIncludes('/docs/network-edge/internal-endpoints'), '/docs/universal-gateway/internal-endpoints'],
+    [ fromIncludes('/docs/network-edge/tls-certificates'), '/docs/universal-gateway/tls-certificates'],  
+    [ fromIncludes('/docs/network-edge/app-users/'), '/docs/traffic-policy/identities/'],
+        // obs   
+    [ fromIncludes('/docs/obs/reference'), '/docs/obs/events/reference'],
+        //tcp
+    [ fromIncludes('/docs/tcp/ip-restrictions'), '/docs/traffic-policy/actions/restrict-ips/'],
+    [ fromIncludes('/docs/tcp/traffic-policy/actions/deny/'), '/docs/traffic-policy/actions/deny/'],
+    [ fromIncludes('/docs/tcp/traffic-policy/actions/'), '/docs/traffic-policy/actions/'],
+    [ fromIncludes('/docs/tcp/traffic-policy/actions/log/'), '/docs/traffic-policy/actions/log'],
+    [ fromIncludes('/docs/tcp/traffic-policy/actions/restrict-ips/'), '/docs/traffic-policy/actions/restrict-ips'],
+        // tls
+    [ fromIncludes('/docs/tls/ip-restrictions/'), '/docs/traffic-policy/actions/restrict-ips'],
+    [ fromIncludes('/docs/tls/mutual-tls/'), '/docs/traffic-policy/actions/terminate-tls/#enabling-mutual-tls'],
+    [ fromIncludes(`/docs/tls/termination/`), '/docs/traffic-policy/actions/terminate-tls/'],
+        // universal gateway
+    [ fromIncludes('/docs/network-edge/gslb/'), '/docs/universal-gateway/global-load-balancer/'],
+    [ fromIncludes('/docs/http/'), '/docs/universal-gateway/http/'],
+    [ fromIncludes('/docs/tcp/'), '/docs/universal-gateway/tcp/'],
+    [ fromIncludes('/docs/tls/'), '/docs/universal-gateway/tls/'],
 ]
 
 // get current href from window
