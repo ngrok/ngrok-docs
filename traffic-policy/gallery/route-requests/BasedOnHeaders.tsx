10d27ffb497d6f3fcc095bdee45b52f51b5e1ef9
Shaquil Hansford <liuqahs15@gmail.com>
2025-02-07 13:41:32 -0500

    Done with add authentication

---
+++ traffic-policy/gallery/route-requests/BasedOnHeaders.tsx
@@ -0,0 +1,16 @@
+import ConfigExample from "../../../src/components/ConfigExample";
+
+export const BasedOnHeaders = () => (
+	<ConfigExample
+		config={{
+			on_http_request: {
+				actions: {
+					type: "forward-internal",
+					config: {
+						url: "https://${getReqHeader('X-Customer-Value')[0]}.internal",
+					},
+				},
+			},
+		}}
+	/>
+);
