f8051ac7e8283b091da68c96a27f5307645a9266
Shaquil Hansford <liuqahs15@gmail.com>
2025-03-14 13:49:30 -0400

    Hashes and redirects for domains

--- static/scripts/redirects/question-answer-redirects.js
+++ static/scripts/redirects/question-answer-redirects.js
@@ -6,6 +6,13 @@ export default [
 	 */
 		// Domains
 	[ fromExact('/docs/universal-gateway/domains/'), '/docs/universal-gateway/domains/what-are-domains/' ],
+			// hashes
+	[ fromExact('/docs/universal-gateway/domains/#branded-domains'), '/docs/universal-gateway/domains/how-do-i-use-my-own-domain/' ],
+	[ fromExact('/docs/universal-gateway/domains/#global-load-balancer'), '/docs/universal-gateway/global-load-balancer/' ],
+	[ fromExact('/docs/universal-gateway/domains/#public-endpoints'), '/docs/universal-gateway/public-endpoints/' ],
+	[ fromExact('/docs/universal-gateway/domains/#ngrok-managed-domains'), '/docs/universal-gateway/domains/what-are-managed-domains/' ],
+	[ fromExact('/docs/universal-gateway/domains/#hsts-preload'), '/docs/universal-gateway/domains/what-are-managed-domains/#hsts-preload' ],
+	[ fromExact('/docs/universal-gateway/domains/#public-suffix-list'), '/docs/universal-gateway/domains/what-are-managed-domains/#public-suffix-list' ],
 	
 		// TCP Addresses
 	[ fromExact('/docs/universal-gateway/tcp-addresses/'), '/docs/universal-gateway/tcp-addresses/what-are-tcp-addresses/' ],
