f8e21f4e9ab106e47c654f4952e3b53b07749209
Shaquil Hansford <liuqahs15@gmail.com>
2025-03-19 14:34:09 -0400

    Done with component; need a demo page

--- src/components/LangSwitcher/index.tsx
+++ src/components/LangSwitcher/index.tsx
@@ -1,33 +1,28 @@
-/**
- * Some important notes:
- * 	- Because we swizzled the codeblock component to return a
- *   DocCodeBlock, we can actually wrap any codeblocks in the docs
- *   with this component and it should work fine. For example:
- *   <CodeSwitcher>
- * 	  ```ts filename="example.ts"
- *     ... code here
- *    ```
- *    ```js filename="example.js"
- * 	   ... code here
- * 	  ```
- *   </CodeSwitcher>
- *   This should work. So at this point it's a matter of adding the tabs
- *   to above the codeblock and only showing the content that makes the tab
- */
 import { parseLanguage, parseMetastring } from "@ngrok/mantle/code-block";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@ngrok/mantle/tabs";
-import DocsCodeBlock from "../code-block.jsx";
+import { titleCase } from "title-case";
+import DocsCodeBlock from "../code-block.tsx";
 
-export function LangSwitcher({
-	children,
-}: {
-	children: (typeof DocsCodeBlock)[];
-}) {
-	console.log("Rendering children", children);
-	// map the children
-	// check the language of the codeblock
-	// generate a tab for each language
-	const structuredChildren = children.map((child: any) => {
+// The name of the query param or localstorage item to search for
+// to get the default tab value
+const paramName = "codeLang";
+
+const getDefaultTab = (structuredChildren: any) => {
+	let defaultValue = localStorage.getItem(paramName);
+	if (!defaultValue) {
+		const searchParams = new URLSearchParams(window.location.search);
+		defaultValue = searchParams.get(paramName);
+	}
+	// Make sure the default value is a valid language for the
+	// code tabs being displayed
+	if (structuredChildren.some((child: any) => child.language === defaultValue))
+		return defaultValue;
+
+	return "";
+};
+
+const getStructuredChildren = (children: any) => {
+	return children.map((child: any) => {
 		const { className, metastring, children } = child.props.children.props;
 		const language = parseLanguage(className);
 		const meta = parseMetastring(metastring);
@@ -39,21 +34,35 @@ export function LangSwitcher({
 			meta,
 		};
 	});
-	console.log("Structured children", structuredChildren);
+};
+
+export function LangSwitcher({
+	children,
+}: {
+	children: (typeof DocsCodeBlock)[];
+}) {
+	console.log("Rendering children", children);
+	const structuredChildren = getStructuredChildren(children);
+
 	return (
 		<Tabs
 			orientation="horizontal"
-			defaultValue={structuredChildren[0]?.language}
-			className="w-[400px]"
+			defaultValue={
+				getDefaultTab(structuredChildren) || structuredChildren[0]?.language
+			}
 		>
 			<TabsList>
-				{structuredChildren.map((child) => (
-					<TabsTrigger key={child.content} value={child.language}>
-						{child.language}
+				{structuredChildren.map((child: any) => (
+					<TabsTrigger
+						onClick={() => localStorage.setItem(paramName, child.language)}
+						key={child.content}
+						value={child.language}
+					>
+						{titleCase(child.language)}
 					</TabsTrigger>
 				))}
 			</TabsList>
-			{structuredChildren.map((child) => (
+			{structuredChildren.map((child: any) => (
 				<TabsContent
 					key={child.content + child.language}
 					value={child.language}
