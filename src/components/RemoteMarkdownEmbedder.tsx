5803e9a03cbde29ed0cb3f339cdd114cbc4e7c37
Shaquil Hansford <liuqahs15@gmail.com>
2025-03-25 09:47:16 -0400

    Add loading text for when the Changelog content isn't ready

--- src/components/RemoteMarkdownEmbedder.tsx
+++ src/components/RemoteMarkdownEmbedder.tsx
@@ -18,6 +18,14 @@ const RemoteMarkdownEmbedder: React.FC<RemoteMarkdownEmbedderProps> = ({
 			.then(setContent);
 	}, [url]);
 
+	if (!content) {
+		return (
+			<pre className="min-h-[3.25rem] p-4 pr-[3.375rem] font-mono text-mono">
+				Loading ...
+			</pre>
+		);
+	}
+
 	const finalContent = textToRemove
 		? content.replace(textToRemove, "")
 		: content;
