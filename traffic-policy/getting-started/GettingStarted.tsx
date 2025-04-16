1e579bc1d0cb77ed3197a00504970c8b23f4f1da
Niji <niji@ngrok.com>
2025-04-16 11:12:54 -0700

    fix: broken traffic-policy example on getting-started
    Fixes #1260

--- traffic-policy/getting-started/GettingStarted.tsx
+++ traffic-policy/getting-started/GettingStarted.tsx
@@ -3,15 +3,15 @@ import ConfigExample from "../../src/components/ConfigExample";
 export const GettingStarted = () => (
 	<ConfigExample
 		config={{
-			on_http_request: {
-				actions: {
+			on_http_request: [{
+				actions: [{
 					type: "custom-response",
 					config: {
 						status_code: 200,
 						content: "Hello, World!",
 					},
-				},
-			},
+				}],
+			}],
 		}}
 	/>
 );
