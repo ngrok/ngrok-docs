1434324d3687ef7d16d31821c7e63aa32dc2cd49
Joel Hans <j.hans@ngrok.com>
2025-05-27 07:54:33 -0700

    Add new pooling video to K8s guides (#1364)
    I pushed out a new K8s-specific pooling video today, so using the new
    `YouTubeEmbed` component to drop it in a few places. Plus, two other
    pools-related tweaks:
    
    - Fixed typos in the pooling doc
    - Changed the link for **Load Balancer** in _What is ngrok?_ to point to
    pools.
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Shaquil Hansford <liuqahs15@gmail.com>

--- src/components/youtube-embed.tsx
+++ src/components/youtube-embed.tsx
@@ -16,7 +16,7 @@ type Props = Omit<ComponentProps<"div">, "children"> & {
 
 function YouTubeEmbed({ className, title, videoId, ...props }: Props) {
 	return (
-		<div className={cx("relative aspect-video", className)} {...props}>
+		<div className={cx("relative aspect-video mb-3", className)} {...props}>
 			<iframe
 				src={`https://www.youtube.com/embed/${videoId}`}
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
