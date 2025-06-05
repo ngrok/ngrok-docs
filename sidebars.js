35afe101f82751e132b5e80d21188cd13bcbbbe0
Shaquil Hansford <liuqahs15@gmail.com>
2025-06-05 15:03:15 -0400

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -656,7 +656,7 @@ const sidebars = {
 				},
 			],
 		},
-		"/guides/security-dev-productivity/security-dev-productivity",
+		"guides/security-dev-productivity/security-dev-productivity",
 		{
 			label: "Other guides",
 			type: "category",
