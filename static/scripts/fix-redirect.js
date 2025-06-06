50e001db8f9592ef050030874c1e2fd4d5c77a9d
Joel Hans <j@joelhans.com>
2025-06-06 11:30:14 -0700

    rm old docs and add redirects

--- static/scripts/fix-redirect.js
+++ static/scripts/fix-redirect.js
@@ -834,10 +834,12 @@ const redirects = [
 	// Kubernetes Operator Revamp
 	[
 		fromExact("/docs/k8s/advanced-deployments/"),
-		"/docs/k8s/installation/install/",
+		"/docs/k8s",
 	],
-	[fromExact("/docs/k8s/deployment-guide/"), "/docs/k8s/installation/install/"],
-	[fromExact("/docs/k8s/developer-guide/"), "/docs/k8s/installation/install/"],
+	[fromExact("/docs/k8s/deployment-guide/"), "/docs/k8s"],
+	[fromExact("/docs/k8s/developer-guide/"), "/docs/k8s"],
+	[fromExact("/docs/k8s/installation/install/"), "/docs/k8s"],
+	[fromExact('/docs/k8s/guides/quickstart/", "/docs/getting-started/kubernetes/ingress"],
 	[
 		fromExact("/docs/k8s/developer-guide/architecture/"),
 		"/docs/k8s/installation/architecture/",
