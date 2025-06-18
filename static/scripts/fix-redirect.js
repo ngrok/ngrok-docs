9fbf19e7b235901a0dcb8ed6cf5f333ababa9361
Shaquil Hansford <liuqahs15@gmail.com>
2025-06-18 13:10:27 -0400

    Undo django

--- static/scripts/fix-redirect.js
+++ static/scripts/fix-redirect.js
@@ -914,6 +914,10 @@ const redirects = [
 		fromExact("/docs/using-ngrok-with/node-js/"),
 		"/docs/getting-started/javascript/",
 	],
+	[
+		fromExact("/docs/using-ngrok-with/django/"),
+		"/docs/getting-started/python/",
+	],
 	[fromExact("/docs/using-ngrok-with/go/"), "/docs/getting-started/go/"],
 	[fromExact("/docs/using-ngrok-with/rust/"), "/docs/getting-started/rust/"],
 	[fromExact("/docs/using-ngrok-with/rdp/"), "/docs/guides/ssh-rdp"],
