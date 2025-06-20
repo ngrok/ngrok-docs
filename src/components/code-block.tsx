dcfae7dc904af3224e7fc332bcd34213f6810871
Shaquil Hansford <liuqahs15@gmail.com>
2025-06-20 13:30:49 -0400

    Update code-block.tsx

--- src/components/code-block.tsx
+++ src/components/code-block.tsx
@@ -5,7 +5,6 @@ import type {
 } from "@ngrok/mantle/code-block";
 import { CodeBlock, parseLanguage } from "@ngrok/mantle/code-block";
 import type { WithStyleProps } from "@ngrok/mantle/types";
-import capitalize from "capitalize";
 import type { ComponentProps, ReactNode } from "react";
 import { CodeBlockWithInfo } from "./CodeBlockWithInfo";
 import { LangTab } from "./LangSwitcher/LangTab";
