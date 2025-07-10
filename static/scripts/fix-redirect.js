ae12c4d9a8e0a090d4cd2423475e97d816a48a78
Russ Savage <russ@ngrok.com>
2025-07-10 14:16:26 -0400

    fix: remove link
    Signed-off-by: Russ Savage <russ@ngrok.com>

--- static/scripts/fix-redirect.js
+++ static/scripts/fix-redirect.js
@@ -410,7 +410,6 @@ const redirects = [
 	],
 	[fromIncludes("/docs/cloud-edge/"), "/docs/universal-gateway/overview/"],
 	[
-		// https://linear.app/ngrok/issue/EPD-1286
 		fromExact("/docs/integrations/home-assistant/home-assistant"),
 		"/docs/integrations/home-assistant/home-assistant-with-ngrok",
 	],
